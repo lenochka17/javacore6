b7abbb4d968d83f62364dd06d5ed90dfa0b4cfe8
lenochka17 <l.batanova17@gmail.com>
2022-02-03 16:14:38 +0300

    Added HomeWork6 Maven

---
+++ src/main/java/org/example/HomeWork6.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+
+public class HomeWork6 {
+    private static final String BASE_HOST = "dataservice.accuweather.com";
+    private static final String FORECAST = "forecasts";
+    private static final String API_VERSION = "v1";
+    private static final String FORECAST_TYPE = "daily";
+    private static final String FORECAST_PERIOD = "5day";
+    private static final String SAINT = "474212_PC";
+    private static final String API_KEY = "aDqTzSeK8COEp3w9ErOFQAUWNHK8qgGp";
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        // Сегментированное построение URL
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(BASE_HOST)
+                .addPathSegment(FORECAST)
+                .addPathSegment(API_VERSION)
+                .addPathSegment(FORECAST_TYPE)
+                .addPathSegment(FORECAST_PERIOD)
+                .addPathSegment(SAINT)
+                .addQueryParameter("apikey", API_KEY)
+                .addQueryParameter("language", "ru-ru")
+                .addQueryParameter("metric", "true")
+                .build();
+
+        System.out.println(url.toString());
+
+        // При необходимости указать заголовки
+        Request requesthttp = new Request.Builder()
+                .addHeader("accept", "application/json")
+                .url(url)
+                .build();
+
+        String jsonResponse = client.newCall(requesthttp).execute().body().string();
+        System.out.println(jsonResponse);
+    }
+
+}
